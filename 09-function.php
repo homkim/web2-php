34616522c8b12dd96bba5595550ae33061eb3212
homkim <homkim01@gmai.com>
2019-06-09 22:01:50 +0900

    09-function.php 를 생성했어요.

---
+++ 09-function.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Function Test</title>
+</head>
+<body>
+  <h1>Function Test</h1>
+  <?php
+     function sum($a, $b)
+    {
+        return $a + $b;
+    }
+
+    echo sum(3, 2);
+  ?>
+</body>
+</html>
